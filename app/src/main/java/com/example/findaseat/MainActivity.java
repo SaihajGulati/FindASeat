6849717c35509f83eb13cecd9412e06b583aee85
Mantej Lamba <mlamba@usc.edu>
2023-11-06 10:47:46 -0800

    map done

--- app/src/main/java/com/example/findaseat/MainActivity.java
+++ app/src/main/java/com/example/findaseat/MainActivity.java
@@ -1,55 +1,39 @@
 package com.example.findaseat;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private GoogleMap myMap;
-    private ImageView mapImageView;
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Set the default launch screen as the "Map" tab
+        loadFragment(new MapFragment());
 
-        mapImageView = findViewById(R.id.mapImageView);
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
-
-        // Set the "Map" tab as the default selected tab
-        bottomNavigationView.setSelectedItemId(R.id.menu_map);
-        // Make the ImageView visible by default
-        mapImageView.setVisibility(View.VISIBLE);
-
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.menu_profile) {
-                    // Handle the Home tab
-                    // You can hide the ImageView if it's visible
-                    mapImageView.setVisibility(View.GONE);
-                    return true;
-                } else if (itemId == R.id.menu_map) {
-                    // Handle the Map tab
-                    // Make the ImageView visible to display the map image
-                    mapImageView.setVisibility(View.VISIBLE);
-                    return true;
-                }
-                // Add conditions for other tabs if needed
-                return false;
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu_map) {
+                loadFragment(new MapFragment());
+                return true;
+            } else if (item.getItemId() == R.id.menu_profile) {
+                loadFragment(new ProfileFragment());
+                return true;
             }
+            return false;
         });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
